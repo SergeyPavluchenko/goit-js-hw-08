6f628b11b43dab29392cf448df79d8ac3fe37aa9
SergeyPavluchenko <s.pavluchenko90@gmail.com>
2022-10-02 23:03:26 +0300

    instal simple-lightbox

--- src/js/01-gallery.js
+++ src/js/01-gallery.js
@@ -1,5 +1,26 @@
-// Add imports above this line
-import { galleryItems } from './gallery-items';
+import { galleryItems } from './gallery-items.js';
 // Change code below this line
+import SimpleLightbox from 'simplelightbox';
+import 'simplelightbox/dist/simple-lightbox.min.css'; 
 
 console.log(galleryItems);
+
+const galleryRef = document.querySelector('.gallery');
+const addGalleryMarkup = createGalleryMarkup(galleryItems);
+
+galleryRef.innerHTML = addGalleryMarkup;
+
+function createGalleryMarkup(items) {
+    return items
+        .map((item) => `<li class="gallery__item"><a class="gallery__link" href="${item.original}">
+            <img class="gallery__image" src="${item.preview}" alt="${item.description}" />
+            </a></li>`
+        )
+        .join('');
+}
+
+const simple = new SimpleLightbox('.gallery a', {
+    captionDelay: 250,
+    captionsData: 'alt'
+});
+
