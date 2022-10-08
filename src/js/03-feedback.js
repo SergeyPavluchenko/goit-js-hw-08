53c6c093c4eb07f1a6d397d51178774d99be3ce8
SergeyPavluchenko <s.pavluchenko90@gmail.com>
2022-10-08 12:30:38 +0300

    Update 03-feedback.js

--- src/js/03-feedback.js
+++ src/js/03-feedback.js
@@ -1,1 +1,11 @@
+import throttle from "lodash.throttle";
+// import localStorage form "./storage"
 
+const formRef = document.querySelector('feedback-form');
+console.log(formRef);
+
+const onFormInput = event => {
+    console.log(event.target);
+}
+
+formRef.addEventListener('input', onFormInput)
