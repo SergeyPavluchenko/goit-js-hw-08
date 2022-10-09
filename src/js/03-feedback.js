780e18472215b25546b9654f39085bd9ce63d62f
SergeyPavluchenko <s.pavluchenko90@gmail.com>
2022-10-09 17:38:20 +0300

    task 3

--- src/js/03-feedback.js
+++ src/js/03-feedback.js
@@ -1,11 +1,46 @@
 import throttle from "lodash.throttle";
-// import localStorage form "./storage"
+import { save, load, remove } from "./storage";
+const formRef = document.querySelector('.feedback-form');
+// console.log(formRef);
 
-const formRef = document.querySelector('feedback-form');
-console.log(formRef);
+const LOCALE_STORAGE_KEY = 'feedback-form-state';
+
+
+initPage();
 
 const onFormInput = event => {
-    console.log(event.target);
+    const { name, value } = event.target;
+
+    let saveData = load(LOCALE_STORAGE_KEY);
+    saveData = saveData ? saveData : {};
+
+    saveData[name] = value;
+
+    save(LOCALE_STORAGE_KEY, saveData)
 }
 
-formRef.addEventListener('input', onFormInput)
+const throttleOnFormInput = throttle(onFormInput, 500)
+formRef.addEventListener('input', onFormInput);
+
+function initPage() {
+    const saveData = load(LOCALE_STORAGE_KEY);
+    console.log(saveData);
+    if (!saveData) {
+        return;
+    }
+    Object.entries(saveData).forEach(([name, value]) => {
+                formRef.elements[name].value = value;
+            })
+}
+
+const hendleSubmit = event => {
+    event.preventDefault();
+    const {
+        elements: {email, message},
+    } = event.currentTarget;
+    console.log({ email: email.value, message: message.value });
+    event.currentTarget.reset();
+    remove(LOCALE_STORAGE_KEY);
+};
+
+formRef.addEventListener('submit', hendleSubmit);
