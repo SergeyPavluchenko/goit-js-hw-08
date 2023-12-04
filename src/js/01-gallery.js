6b130b33c05deb691632d2146db17334f1ed4da3
Sergey <s.pavluchenko90@gmail.com>
2023-12-04 23:43:13 +0200

    test

--- src/js/01-gallery.js
+++ src/js/01-gallery.js
@@ -1,26 +1,5 @@
-import { galleryItems } from './gallery-items.js';
+// Add imports above this line
+import { galleryItems } from './gallery-items';
 // Change code below this line
-import SimpleLightbox from 'simplelightbox';
-import 'simplelightbox/dist/simple-lightbox.min.css';
 
 console.log(galleryItems);
-
-const galleryRef = document.querySelector('.gallery');
-const addGalleryMarkup = createGalleryMarkup(galleryItems);
-
-galleryRef.innerHTML = addGalleryMarkup;
-
-function createGalleryMarkup(items) {
-  return items
-    .map(
-      item => `<a class="gallery__link" href="${item.original}">
-            <img class="gallery__image" src="${item.preview}" alt="${item.description}" />`
-    )
-    .join('');
-}
-
-const simple = new SimpleLightbox('.gallery a', {
-  captionDelay: 250,
-  captionsData: 'alt',
-});
-console.log(simple);
